b927e339f939ae0fffa99a2402312c210156c79f
Kjartan Hrafnkelsson <kjh14@hi.is>
2025-04-12 17:40:43 +0000

    patch(routes/404): implement HEAD request; send HTML in 405 response

--- routes/404.ts
+++ routes/404.ts
@@ -1,9 +1,25 @@
 import type { T_Http_Request, T_Http_Response } from "@/lib/http.ts";
 import { render } from "@/lib/views.ts";
+import { min_status_response } from "@/lib/html.ts";
 
 export default async function handler(
     req: T_Http_Request,
 ): Promise<T_Http_Response> {
+    if (req.method === "HEAD") {
+        return req.respond({
+            status: 404,
+            headers: { content_type: "text/html" },
+        });
+    }
+
+    if (req.method !== "GET") {
+        return req.respond({
+            status: 405,
+            headers: { content_type: "text/html" },
+            body: min_status_response(405),
+        });
+    }
+
     if (req.pathname.startsWith("/is")) {
         return req.respond({
             status: 404,
